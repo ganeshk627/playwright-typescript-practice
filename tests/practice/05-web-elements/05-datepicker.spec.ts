52bb4ec0e193e946f7c079e71d0eb11b54695722
Ganesh K <ganeshkannan627@gmail.com>
2024-06-18 10:34:36 +0530

    updating datepicker and alerts

--- tests/practice/05-web-elements/05-datepicker.spec.ts
+++ tests/practice/05-web-elements/05-datepicker.spec.ts
@@ -1,7 +1,53 @@
 import { test, expect } from '@playwright/test';
 
-test('Date Picker', async ({ page }) => {
-  await page.goto('https://www.lambdatest.com/selenium-playground/javascript-alert-box-demo');
-  
+var moment = require('moment');
 
+const dob1 = '05061999';
+const dob2 = '05-06-1999';
+const dob3 = '05/06/1999';
+const dob4 = '1999-06-05'; // YYYY-MM-DD only applicable with playwright
+const dob5 = '19990605';
+const dob6 = '1999-06-05 T18:37:46.152Z';
+
+
+test('Date Picker using fill', async ({ page }) => {
+  await page.goto('https://www.lambdatest.com/selenium-playground/bootstrap-date-picker-demo');
+
+  await page.waitForTimeout(2000);
+  await page.locator('#birthday').fill(dob4); // format should be YYYY-MM-DD
+  await expect(page.locator('#birthday')).toHaveValue('1999-06-05');
 });
+
+test('Date Picker using pressSequentially', async ({ page }) => {
+  await page.goto('https://www.lambdatest.com/selenium-playground/bootstrap-date-picker-demo');
+
+  await page.waitForTimeout(2000);
+  await page.locator('#birthday').pressSequentially(dob2);
+  await expect(page.locator('#birthday')).toHaveValue('1999-06-05');
+});
+
+test('Date Picker using multiple clicks', async ({ page }) => {
+
+  let monthYear = 'June 2023';
+  let day = 5;
+
+  await page.goto('https://www.lambdatest.com/selenium-playground/bootstrap-date-picker-demo');
+  await page.waitForTimeout(2000);
+
+
+  await page.locator('input[placeholder="Start date"]').click();
+  await page.waitForTimeout(1000);
+
+  while (await page.locator('.datepicker-switch').first().textContent() != monthYear) {
+    if (moment(monthYear, 'MMMM YYYY').isBefore()) {
+      await page.locator('.prev').first().click();
+    } else {
+      await page.locator('.next').first().click();
+    }
+    await page.waitForTimeout(1000);
+  }
+  await page.locator('//td[@class="day"][text()="' + day + '"]').click();
+
+  await expect(page.locator('input[placeholder="Start date"]')).toHaveValue('05/06/2023');
+});
+
--- tests/practice/05-web-elements/07-alerts.spec.ts
+++ tests/practice/05-web-elements/07-alerts.spec.ts
@@ -3,18 +3,42 @@ import { test, expect } from '@playwright/test';
 test('Simple Alert - No Handle', async ({ page }) => {
   await page.goto('https://www.lambdatest.com/selenium-playground/javascript-alert-box-demo');
 
+  await page.waitForTimeout(1000);
+  await page.locator('//p[text()="JavaScript Alerts"]/button').click();
 
 });
 
 test('Simple Alert', async ({ page }) => {
   await page.goto('https://www.lambdatest.com/selenium-playground/javascript-alert-box-demo');
 
+  page.on('dialog', async dialog => {
+    console.log(dialog.message())
+    console.log(dialog.type())
+    expect(dialog.message()).toContain('I am an alert boxes!')
+    await dialog.accept();
+  });
+
+  await page.waitForTimeout(1000);
+  await page.locator('//p[text()="JavaScript Alerts"]/button').click();
+
+  await page.waitForTimeout(1000);
 
 });
 
 
 test('Confirm Alert - Ok', async ({ page }) => {
+
   await page.goto('https://www.lambdatest.com/selenium-playground/javascript-alert-box-demo');
+
+  page.on('dialog', async dialog => {
+    console.log(dialog.message())
+    console.log(dialog.type())
+    // expect(dialog.message()).toContain('I am an alert boxes!')
+    await dialog.accept();
+  });
+
+  await page.waitForTimeout(1000);
+  await page.locator('//p[text()="Confirm box:"]/button').click();
 
 
 });
@@ -24,10 +48,29 @@ test('Confirm Alert - Cancel', async ({ page }) => {
   await page.goto('https://www.lambdatest.com/selenium-playground/javascript-alert-box-demo');
 
 
+  page.on('dialog', async dialog => {
+    console.log(dialog.message())
+    console.log(dialog.type())
+    // expect(dialog.message()).toContain('I am an alert boxes!')
+    await dialog.dismiss();
+  });
+
+  await page.waitForTimeout(1000);
+  await page.locator('//p[text()="Confirm box:"]/button').click();
+
 });
 
 test('Prompt Alert', async ({ page }) => {
   await page.goto('https://www.lambdatest.com/selenium-playground/javascript-alert-box-demo');
+  page.on('dialog', async dialog => {
+    console.log(dialog.message())
+    console.log(dialog.type())
+    // expect(dialog.message()).toContain('I am an alert boxes!')
+    await dialog.accept('Harry!');
+  });
+
+  await page.waitForTimeout(1000);
+  await page.locator('//p[text()="Prompt box:"]/button').click();
 
 
 });
